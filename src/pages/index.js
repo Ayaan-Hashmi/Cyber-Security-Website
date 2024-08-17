98bf612a290a116fa77395582bb36ab6d8d61adf
whirlxd <ninja21official@gmail.com>
2024-08-18 01:06:33 +0530

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,5 @@
-export { default as cybersecurity } from "./Cybersec";
 export { default as FormValidation } from "./FormValidation";
+export { default as env } from "./ENV";
+export { default as flv } from "./File Input Validation";
+export { default as lfi } from "./FormValidation";
+export { default as sql } from "./Cyber Hygiene";
